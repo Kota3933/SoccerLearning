7202d68f1e549eb85a6846a439cf32727238e327
kota <ortega3933@gmail.com>
2025-04-06 14:43:25 +0900

    タグ表示の修正

--- src/app/watch/[watch]/Tags.tsx
+++ src/app/watch/[watch]/Tags.tsx
@@ -1,7 +1,55 @@
-import { Chip } from "@mui/material";
+import { Chip, Paper, styled } from "@mui/material";
 
-export default function Tags() {
+type Chip = {
+	key: number,
+	kind: string,
+	label: string
+}
+
+const ListItem = styled('li')(({ theme }) => ({
+	margin: theme.spacing(0.5),
+  }));
+
+export default function Tags({ data }) {
+
+	const lectureData = data;
+	const chipData:Chip[] = [];
+	let positionNum = 0;
+
+	lectureData.lecture.position.map((position:string, index:number) => {
+		chipData.push({
+			key: index,
+			kind: 'position',
+			label: position
+		})
+		positionNum = index;
+	})
+
+	lectureData.lecture.skill.map((skill:string, index:number) => {
+		chipData.push({
+			key: positionNum+index+1,
+			kind: 'skill',
+			label: skill
+		})
+	})
+
+	// const chipData = [
+	// 	{ key: 0, kind: 'position', label: 'センターバック（CB）' },
+	// 	{ key: 1, kind: 'position', label: 'サイドバック/フルバック（SB/FB）' },
+	// 	{ key: 2, kind: 'skill', label: 'パス' },
+	// 	{ key: 3, kind: 'skill', label: 'ボールコントロール' },
+	// ]
 	return (
-		<Chip label="ドリブル" />
+		<main className="tagPaper">
+			<Paper sx={{display: 'flex', flexWrap: 'wrap', listStyle: 'none', p: 0, m:0, width: 'fit-content'}} component='ul'>
+				{chipData.map((data) => {
+					return (
+						<ListItem key={data.key}>
+							<Chip label={data.label} color={data.kind === 'position' ? 'primary' : 'secondary'} size="small" />
+						</ListItem>
+					)
+				})}
+			</Paper>
+		</main>		
 	)
 }
